e2983b1131dfba76f8a53b95c1b475cf6d7d5f6d
Hamza Akhtar <hamza019akhtar@gmail.com>
2018-04-09 14:49:04 +0530

    Test Commit to local repo

--- src/main/java/com/springdemo1/controller/HomeController.java
+++ src/main/java/com/springdemo1/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.springdemo1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,10 @@ public class HomeController {
 	public String getGreeting() {
 		return "Welcome to first spring boot application";
 	}
+	
+	@RequestMapping("/message")
+	public String getMessage() {
+		return "The world is beautiful! ";
+	}
+	
 }
